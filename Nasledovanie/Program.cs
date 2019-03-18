47594c058bd32eced79673fa3bcbaad6dadfdb77
AlexandrTolstov <alextav_85@mail.ru>
2019-03-18 20:26:36 +0600

    Add project files.

---
+++ Nasledovanie/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasledovanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
